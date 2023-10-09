ed911c9b4874fdb06bf7a2f9877d81398a3d4226
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-10-08 23:10:39 -0300

    feat/addingTasksTableOnDashBoard :art: :lipstick:

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,15 +1,35 @@
+import { TaskSchema } from '../../hooks/useGetTasks'
+import { TaskTr } from '../AllTasks/components/TaskTr'
 import { ResumeCard } from './components/ResumeCard'
 import {
   CardsArea,
   Container,
   DashBoardHeader,
   FlexArea,
+  ListViewTable,
+  ListViewTableBody,
+  ListViewTableHeader,
   Notifications,
+  TasksForToday,
+  TasksForTomorrow,
   TasksResume,
   TextHeader,
+  TitleOfBox,
   WelcomeIcon,
   WelcomePhrase,
 } from './styles'
+
+const fakeTask: TaskSchema = {
+  id: '9a92b59e-aea8-41c1-a83e-054c8f0311b0',
+  createdAt: new Date('2023-10-05T19:40:01.000Z'),
+  maturity: new Date('2023-10-05T19:40:01.000Z'),
+  completedAt: new Date('2023-10-05T19:40:01.000Z'),
+  title: 'Task 1',
+  status: 'opened',
+  priority: 'normal',
+  description: '',
+  userId: '4cfd03a1-addf-4941-a488-a5f1d2ca6fbe',
+}
 
 export function DashBoard() {
   return (
@@ -32,8 +52,59 @@ export function DashBoard() {
         <ResumeCard amount={51} status="concluded" />
       </CardsArea>
       <FlexArea>
-        <TasksResume></TasksResume>
-        <Notifications></Notifications>
+        <TasksResume>
+          <TasksForToday>
+            <TitleOfBox>
+              <span>Tarefas para hoje:</span>
+            </TitleOfBox>
+            <ListViewTable>
+              <ListViewTableHeader>
+                <tr>
+                  <th>Título</th>
+                  <th>Status</th>
+                  <th>Deadline</th>
+                  <th>Prioridade</th>
+                </tr>
+              </ListViewTableHeader>
+              <ListViewTableBody>
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+              </ListViewTableBody>
+            </ListViewTable>
+          </TasksForToday>
+          <TasksForTomorrow>
+            <TitleOfBox>
+              <span>Tarefas para amanhã:</span>
+            </TitleOfBox>
+            <ListViewTable>
+              <ListViewTableHeader>
+                <tr>
+                  <th>Título</th>
+                  <th>Status</th>
+                  <th>Deadline</th>
+                  <th>Prioridade</th>
+                </tr>
+              </ListViewTableHeader>
+              <ListViewTableBody>
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+                <TaskTr task={fakeTask} />
+              </ListViewTableBody>
+            </ListViewTable>
+          </TasksForTomorrow>
+        </TasksResume>
+        <Notifications>
+          <TitleOfBox>
+            <span>Notificações:</span>
+          </TitleOfBox>
+        </Notifications>
       </FlexArea>
     </Container>
   )
--- src/pages/DashBoard/styles.ts
+++ src/pages/DashBoard/styles.ts
@@ -48,12 +48,57 @@ export const FlexArea = styled.section`
 `
 
 export const TasksResume = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-rows: 2fr 1fr;
+  gap: 1.5rem;
+  overflow: auto;
+`
+
+export const TasksForToday = styled.div`
   background-color: ${(props) => props.theme.cardBgSecondary};
   border: 1px solid ${(props) => props.theme.borderCard};
   padding: 1rem;
   border-radius: 6px;
   height: 100%;
+  overflow: auto;
 `
+
+export const TasksForTomorrow = styled.div`
+  background-color: ${(props) => props.theme.cardBgSecondary};
+  border: 1px solid ${(props) => props.theme.borderCard};
+  padding: 1rem;
+  border-radius: 6px;
+  height: 100%;
+  overflow: auto;
+`
+
+export const TitleOfBox = styled.div`
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid ${(props) => props.theme.textSecondary};
+
+  span {
+    font-size: 1rem;
+    font-weight: bold;
+  }
+`
+
+export const ListViewTable = styled.table`
+  width: 99%;
+  border-collapse: separate;
+  border-spacing: 0 1rem;
+`
+
+export const ListViewTableHeader = styled.thead`
+  tr {
+    th {
+      font-weight: 300;
+      text-align: start;
+    }
+  }
+`
+
+export const ListViewTableBody = styled.tbody``
 
 export const Notifications = styled.div`
   background-color: ${(props) => props.theme.cardBgSecondary};
