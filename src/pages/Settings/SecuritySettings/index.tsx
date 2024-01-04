1c392d62952b0da16d3c4de0403594fdc4c1a06e
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-01-04 11:37:02 -0300

    feat/editPasswordFlow :art:

--- src/pages/Settings/SecuritySettings/index.tsx
+++ src/pages/Settings/SecuritySettings/index.tsx
@@ -12,11 +12,23 @@ import {
 import { Button } from '../../../components/Button'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { api } from '../../../lib/axios'
+import { ToastContainer } from 'react-toastify'
+import { useNotify } from '../../../hooks/useNotify'
 
 const formSchema = z.object({
-  password: z.string().nonempty(),
-  newPassword: z.string().nonempty(),
-  confirmNewPassword: z.string().nonempty(),
+  password: z
+    .string()
+    .min(6, { message: 'Mínimo de 6 caracteres' })
+    .nonempty({ message: 'Campo Obrigatório' }),
+  newPassword: z
+    .string()
+    .min(6, { message: 'Mínimo de 6 caracteres' })
+    .nonempty({ message: 'Campo Obrigatório' }),
+  confirmNewPassword: z
+    .string()
+    .min(6, { message: 'Mínimo de 6 caracteres' })
+    .nonempty({ message: 'Campo Obrigatório' }),
 })
 
 type FormSchema = z.infer<typeof formSchema>
@@ -25,12 +37,33 @@ export function SecuritySettings() {
   const methods = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
   })
+  const { notify } = useNotify()
+
+  const onSubmit = methods.handleSubmit(async (data) => {
+    const newPasswordMatch = data.newPassword === data.confirmNewPassword
+
+    if (newPasswordMatch) {
+      try {
+        await api.put('/users/edit-password', {
+          password: data.newPassword,
+          oldPassword: data.password,
+        })
+        notify({ type: 'sucess', message: 'Senha alterada com Sucesso!' })
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        notify({
+          type: 'error',
+          message: err.response.data.message || 'Erro desconhecido',
+        })
+      }
+    }
+  })
 
   return (
     <Container>
       <PasswordArea>
         <FormProvider {...methods}>
-          <StyledForm>
+          <StyledForm onSubmit={onSubmit}>
             <FormTitle>Alterar senha:</FormTitle>
             <FieldsArea>
               <FlexArea>
@@ -38,12 +71,21 @@ export function SecuritySettings() {
                   name="password"
                   label="Senha Atual:"
                   isRequired
+                  errorMessage={methods.formState.errors.password?.message}
                 />
-                <InputPassword name="password" label="Nova Senha:" isRequired />
                 <InputPassword
-                  name="password"
+                  name="newPassword"
+                  label="Nova Senha:"
+                  isRequired
+                  errorMessage={methods.formState.errors.newPassword?.message}
+                />
+                <InputPassword
+                  name="confirmNewPassword"
                   label="Confirmação de Senha:"
                   isRequired
+                  errorMessage={
+                    methods.formState.errors.confirmNewPassword?.message
+                  }
                 />
               </FlexArea>
             </FieldsArea>
@@ -58,6 +100,7 @@ export function SecuritySettings() {
           </StyledForm>
         </FormProvider>
       </PasswordArea>
+      <ToastContainer />
     </Container>
   )
 }
