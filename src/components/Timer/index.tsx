67bd6518155bd2483ccf476af93d4da7da3ea097
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-01-26 00:09:20 -0300

    feat/createStopWatch :art:

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -6,40 +6,55 @@ import {
   TimerDescription,
   TimerLabel,
 } from './styles'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export function Timer() {
   const [currentTime, setCurrentTime] = useState(0)
   const [started, setStarted] = useState(false)
 
-  const handleToggleTimer = useCallback(() => {
-    if (!started) {
-      setStarted(true)
-      setInterval(() => {
-        setCurrentTime(() => currentTime + 1)
+  const intervalRef = useRef<number | null>(null)
+
+  const handleToggleStopWatch = useCallback(() => {
+    setStarted((currentState) => !currentState)
+  }, [])
+
+  useEffect(() => {
+    if (started) {
+      intervalRef.current = window.setInterval(() => {
+        setCurrentTime((prevSeconds) => prevSeconds + 1)
       }, 1000)
+    } else {
+      clearInterval(intervalRef.current!)
     }
 
-    if (started) {
-      setStarted(false)
-    }
-  }, [currentTime, started])
+    return () => clearInterval(intervalRef.current!)
+  }, [started])
+
+  const formatTime = useCallback((time: number): string => {
+    const hours = Math.floor(time / 3600)
+    const minutes = Math.floor((time % 3600) / 60)
+    const secs = time % 60
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(
+      2,
+      '0',
+    )}:${String(secs).padStart(2, '0')}`
+  }, [])
 
   return (
     <Container>
       <TimerLabel>Timer:</TimerLabel>
       <MainContainer>
         {started ? (
-          <IconArea onClick={handleToggleTimer}>
-            <Pause />
+          <IconArea>
+            <Pause onClick={handleToggleStopWatch} />
           </IconArea>
         ) : (
-          <IconArea onClick={handleToggleTimer}>
-            <Play />
+          <IconArea>
+            <Play onClick={handleToggleStopWatch} />
           </IconArea>
         )}
         <TimerDescription id="timerDescription" $isStarted={started}>
-          <span>{currentTime}</span>
+          <span>{formatTime(currentTime)}</span>
         </TimerDescription>
       </MainContainer>
     </Container>
