e5005228f1477cd9c79121293ec4c5d05f7f95e4
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-02-12 14:56:49 -0300

    feat/refactorTaskTr :art:

--- src/components/TaskTr/style.ts
+++ src/components/TaskTr/style.ts
@@ -1,3 +1,4 @@
+import hexToRgba from 'hex-to-rgba'
 import styled, { css } from 'styled-components'
 
 interface StatusContainerProps {
@@ -10,18 +11,25 @@ interface PriorityContainer {
 
 export const TaskItem = styled.div`
   width: 100%;
-  padding: 0.75rem;
-  border-radius: 6px;
+  min-height: fit-content;
   background-color: ${(props) => props.theme.cardBgSecondary};
+  border: 1px solid ${(props) => props.theme.borderCard};
+  border-radius: 6px;
+  padding: 0.5rem 1rem;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
 
-  display: grid;
-  grid-template-columns: 2fr 1fr 1fr 1fr;
+  display: flex;
+  gap: 0.75rem;
+
+  &:hover {
+    background-color: ${(props) => hexToRgba(props.theme.cardBgPrimary, 0.5)};
+  }
 `
 
 export const DescriptionContainer = styled.div`
   display: flex;
+  flex: 1;
   align-items: center;
   gap: 0.5rem;
 `
@@ -35,7 +43,7 @@ export const StatusContainer = styled.div<StatusContainerProps>`
 
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.25rem;
 
   ${(props) =>
     props.$status === 'toDo' &&
@@ -113,7 +121,7 @@ export const MaturityContainer = styled.div`
 
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.25rem;
 
   svg {
     width: 1rem;
@@ -131,7 +139,7 @@ export const PriorityContainer = styled.div<PriorityContainer>`
 
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.25rem;
 
   svg {
     width: 1rem;
@@ -160,4 +168,152 @@ export const PriorityContainer = styled.div<PriorityContainer>`
         color: ${(props) => props.theme.error};
       `}
   }
+`
+
+interface DropDownBoxProps {
+  $isOpen: boolean
+}
+
+export const CustomDropDown = styled.div<DropDownBoxProps>`
+  background-color: ${(props) => props.theme.cardBgPrimary};
+  border: 1px solid ${(props) => props.theme.borderCard};
+  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.2);
+  display: none;
+  position: absolute;
+  padding: 0.35rem;
+  margin-top: 0.25rem;
+  border-radius: 6px;
+
+  flex-direction: column;
+  gap: 0.35rem;
+
+  ${(props) =>
+    props.$isOpen &&
+    css`
+      display: flex;
+    `}
+`
+
+interface StatusOptionProps {
+  $status: string
+  $isActive: boolean
+}
+
+export const StatusOption = styled.div<StatusOptionProps>`
+  font-size: 0.65rem;
+  background-color: ${(props) => hexToRgba(props.theme.borderCard, 0.3)};
+  border-radius: 6px;
+  padding: 0 0.25rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+
+  transition: all 0.2s ease-in-out;
+
+  svg {
+    ${(props) =>
+      props.$status === 'toDo' &&
+      css`
+        color: ${(props) => props.theme.opened};
+      `}
+
+    ${(props) =>
+      props.$status === 'standby' &&
+      css`
+        color: ${(props) => props.theme.stand_by};
+      `}
+
+    ${(props) =>
+      props.$status === 'inProgress' &&
+      css`
+        color: ${(props) => props.theme.in_progress};
+      `}
+
+      ${(props) =>
+      props.$status === 'approval' &&
+      css`
+        color: ${(props) => props.theme.approval};
+      `}
+
+    ${(props) =>
+      props.$status === 'payment' &&
+      css`
+        color: ${(props) => props.theme.payment};
+      `}
+
+      ${(props) =>
+      props.$status === 'concluded' &&
+      css`
+        color: ${(props) => props.theme.concluded};
+      `}
+  }
+
+  ${(props) =>
+    props.$isActive
+      ? css`
+          background-color: ${(props) => props.theme.enphasis};
+        `
+      : css`
+          &:hover {
+            background-color: ${(props) =>
+              hexToRgba(props.theme.borderCard, 0.8)};
+          }
+        `}
+`
+
+interface PriorityOptionProps {
+  $priority: string
+  $isActive: boolean
+}
+
+export const PriorityOption = styled.div<PriorityOptionProps>`
+  font-size: 0.65rem;
+  background-color: ${(props) => hexToRgba(props.theme.borderCard, 0.2)};
+  border-radius: 6px;
+  padding: 0 0.25rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+
+  transition: all 0.2s ease-in-out;
+
+  svg {
+    ${(props) =>
+      props.$priority === 'low' &&
+      css`
+        color: ${(props) => props.theme.sucess};
+      `}
+
+    ${(props) =>
+      props.$priority === 'normal' &&
+      css`
+        color: ${(props) => props.theme.enphasis};
+      `}
+
+    ${(props) =>
+      props.$priority === 'high' &&
+      css`
+        color: ${(props) => props.theme.danger};
+      `}
+
+      ${(props) =>
+      props.$priority === 'urgent' &&
+      css`
+        color: ${(props) => props.theme.error};
+      `}
+  }
+
+  ${(props) =>
+    props.$isActive
+      ? css`
+          background-color: ${(props) => props.theme.enphasis};
+        `
+      : css`
+          &:hover {
+            background-color: ${(props) =>
+              hexToRgba(props.theme.borderCard, 0.8)};
+          }
+        `}
 `
